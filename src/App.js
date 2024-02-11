2561ea48cb352f32bcbfb42e1639c88621dc9f0b
titansofbattle <155274869+titansofbattle@users.noreply.github.com>
2024-02-11 00:07:41 -0800

    Develop (#6)
    * adding landing page
    
    * retry deployment
    
    * updating text
    
    * making adjustments to mobile view
    
    * adjusting mobile top spacing
    
    * adding uniswap link
    
    * adding links
    
    * target blank

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import left from './assets/left.webp'
 import main from './assets/main.webp'
 import right from './assets/right.webp'
 
+
 function App() {
   return (
     <section>
@@ -13,9 +14,14 @@ function App() {
             <h1 className="text-xl md:text-xl text-yellow-300 tracking-loose">Dragon Mint Advisors</h1>
             <h2 className="hidden text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Headline</h2>
             <p className="text-sm md:text-base text-gray-50 mb-4">Unlock a World of Digital Excellence in ERC404 with Dragon Mint Advisors</p>
-            <a href="#"
+            <a target="_blank" href="https://app.uniswap.org/swap?inputCurrency=0xF19308F923582A6f7c465e5CE7a9Dc1BEC6665B1&outputCurrency=0x2c47e4ad6C1c512c5890877f6a3108431d79A53f"
               className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
             Claim Yours Now</a>
+            <a className="hover:scale-125 duration-300 mt-4" href="https://t.me/dragonmintadvisors" target="_blank" rel="noreferrer" aria-label="telegram">
+              <svg className="fill-current h-7 text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314 4.692c.46 0 .663-.211.921-.46l2.211-2.15 4.599 3.397c.848.467 1.457.227 1.668-.785l3.019-14.228c.309-1.239-.473-1.8-1.282-1.434z"></path>
+              </svg>
+            </a>
           </div>
           <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center">
             <div className="h-48 flex flex-wrap content-center">
